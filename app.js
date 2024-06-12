0a4904589bc78e288793d0ea793c800a1d45c55e
helloag-p <parvagarwal04@gmail.com>
2024-06-12 19:57:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
-app.get('/signup',function(req,res){
+app.get('/',function(req,res){
     res.render("signup");
 })
 const { handleLogin } = require("./controllers/login");
